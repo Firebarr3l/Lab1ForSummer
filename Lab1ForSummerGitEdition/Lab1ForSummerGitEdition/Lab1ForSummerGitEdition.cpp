d34e2a4f49eb66fbe8f1b488b5fd0a257fe3c124
Daniil Klishin <Daniilklishin17@mail.ru>
2024-06-11 17:33:16 +0300

    Начало, создание проекта

---
+++ Lab1ForSummerGitEdition/Lab1ForSummerGitEdition/Lab1ForSummerGitEdition.cpp
@@ -0,0 +1,11 @@
+// Lab1ForSummerGitEdition.cpp: главный файл проекта.
+
+#include "stdafx.h"
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+    Console::WriteLine(L"Здравствуй, мир!");
+    return 0;
+}
