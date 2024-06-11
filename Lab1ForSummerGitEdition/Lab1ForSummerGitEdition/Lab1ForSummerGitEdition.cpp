a040ef63d1cebc7ae2625f3f9fa5607bbba10549
Daniil Klishin <Daniilklishin17@mail.ru>
2024-06-11 17:38:50 +0300

    Дабовление используемых библеотек, а также дефаинов

--- Lab1ForSummerGitEdition/Lab1ForSummerGitEdition/Lab1ForSummerGitEdition.cpp
+++ Lab1ForSummerGitEdition/Lab1ForSummerGitEdition/Lab1ForSummerGitEdition.cpp
@@ -1,11 +1,22 @@
 // Lab1ForSummerGitEdition.cpp: главный файл проекта.
+#include "stdafx.h"    // Подключение стандартного заголовочного файла
+#include <stdio.h>     // Подключение библиотеки для работы с вводом-выводом
+#include <stdlib.h>    // Подключение стандартной библиотеки для работы с памятью и преобразованиями
+#include <conio.h>     // Подключение библиотеки для работы с консольным вводом-выводом
+#include <string.h>    // Подключение библиотеки для работы со строками
+#include <malloc.h>    // Подключение библиотеки для работы с динамическим распределением памяти
+#include <clocale>     // Подключение библиотеки для работы с локалью
+#include <windows.h>   // Подключение библиотеки Windows API
 
-#include "stdafx.h"
+using namespace std; 
+using namespace System;  
+using namespace System::IO;  // Использование пространства имен System::IO  
+ 
+#define ENTER 13  // Определение клавиши Enter
+#define ESC 27  // Определение клавиши Escape
+#define UP 72  // Определение клавиши стрелки вверх
+#define DOWN 80  // Определение клавиши стрелки вниз
+#define HOME 71 // Определение клавиши Home
+#define END 79 // Определение клавиши End
+#define LENGTHMENU 84 // Определение длины меню
 
-using namespace System;
-
-int main(array<System::String ^> ^args)
-{
-    Console::WriteLine(L"Здравствуй, мир!");
-    return 0;
-}
